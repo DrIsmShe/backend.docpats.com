063712a03d78198ef86536bf920972035584cf88
DrIsmShe <gitaccount@mail.ru>
2026-04-27 18:44:37 +0400

    fix(models): remove callLog1 duplicate, add mongoose import/export to Consent

--- common/models/DoctorVerification/Consent.js
+++ common/models/DoctorVerification/Consent.js
@@ -1,12 +1,20 @@
-const ConsentSchema = new mongoose.Schema({
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+import mongoose from "mongoose";
 
-  type: {
-    type: String,
-    enum: ["data_processing", "ai_disclaimer", "terms"],
+const ConsentSchema = new mongoose.Schema(
+  {
+    doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+    type: {
+      type: String,
+      enum: ["data_processing", "ai_disclaimer", "terms"],
+    },
+
+    acceptedAt: Date,
+    ip: String,
+    userAgent: String,
   },
+  { timestamps: true },
+);
 
-  acceptedAt: Date,
-  ip: String,
-  userAgent: String,
-});
+const Consent = mongoose.model("Consent", ConsentSchema);
+export default Consent;
